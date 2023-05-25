84a5cec82ae5edf23251cc23b592570188f5f0e5
= <=>
2023-05-25 00:40:40 -0300

    feat: Mongodb - Conexão e primeiro Model

--- nodejs_curso/aula_6_express/server.js
+++ nodejs_curso/aula_6_express/server.js
@@ -1,27 +1,43 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const path = require('path');
+
+//importando o gerenciador mongodb
+const mongoose = require('mongoose');
+
 const { middlewareGlobal, outroMiddleware } = require('./src/middlewares/middleware');
+
+//criando connexão + emitindo atravez de uma promisse quando a conexão estará pronta
+mongoose.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true,useUnifiedTopology: true})
+    .then(() => {
+        app.emit("pronto")
+    })
+    .catch( e => console.log(e))
+
 // rotas
 const routes = require("./router");
+
 //usando metodos e modulos importados. 
 app.use(express.urlencoded({extended:true}));
+
 //conteudo statico
 app.use(express.static(path.resolve(__dirname,'public')));
 
 //pegando caminho absoluto para pasta views 
 app.set("views",path.resolve(__dirname,'src','views'));
+
 //usando a engine ejs
 app.set('view engine', 'ejs');
 app.use(middlewareGlobal);
 app.use(outroMiddleware)
 app.use(routes);
 
-
-
-
-//definindo porta
-app.listen(3000,() =>{
-    console.log('Acessar http://localhost:3000');
-    console.log("servidor executando na porta 3000");
-});
+//conectando no servidor quando a conexão estiver pronta
+app.on('pronto', () => {
+    //definindo porta
+    app.listen(3000, () => {
+        console.log('Acessar http://localhost:3000');
+        console.log("servidor executando na porta 3000");
+    });
+})
